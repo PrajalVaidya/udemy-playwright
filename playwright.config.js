96a6ad9ecb606104863d59f43918bbd88a394248
Prajal <87979800+PrajalVaidya@users.noreply.github.com>
2025-05-06 00:15:27 +0545

    changes on config file

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,6 @@
 // @ts-check
-import { defineConfig, devices } from '@playwright/test';
+const { defineConfig, devices } = require('@playwright/test');
+
 
 /**
  * Read environment variables from file.
@@ -35,7 +36,8 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
+    screenshot:'on'
   },
 
   /* Configure projects for major browsers */
